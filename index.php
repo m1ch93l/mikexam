b3ef1132cc451104d5a851c423e6de31d310c6ea
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-09-19 22:35:49 +0800

    alter index

--- index.php
+++ index.php
@@ -12,6 +12,33 @@ if (isset($_SESSION['participant'])) {
 <?php include 'includes/header.php'; ?>
 
 <body>
-    
+    <div class="container-fluid d-flex align-items-center justify-content-center vh-100">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-header">
+                        <h3 class="card-title">mikexam</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="login" method="POST">
+                            <div class="form-group">
+                                <label for="participant">Username</label>
+                                <input type="text" class="form-control" id="participant" name="participant" autofocus>
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password">
+                                <br>
+                                <button type="submit" class="form-control btn btn-primary" name="login">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
+
 </html>
