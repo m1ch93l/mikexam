24c3f14ef6538ffe91ab044111c343f76f4ca250
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-09-19 22:19:34 +0800

    Links bootstrap.

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>mikexam</title>
+    <!-- tab icon -->
+    <link rel="shortcut icon" href="">
+    <!-- bootstrap ui -->
+    <link href="mikexam/bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="mikexam/bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</head>
