8b37baf2a4057c7c29e4da28d8b143f2f78af559
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-09-19 22:36:15 +0800

    add home page.

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+
+<body>
+    <header class="bg-dark py-3 text-white text-center">
+mikexam
+<div class="float-end h6 me-3 border p-1 text-capitalize">logout</div>
+    </header>
+    <div class="container-md">
+        <h1 class="text-center text-capitalize">Welcome, <?= $_SESSION['fullname'] ?></h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            sakdljkas
+        </div>
+    </div>
+</body>
+
+</html>
