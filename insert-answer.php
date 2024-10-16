1a73f31326f40ec55cfa0560d9b0e61114643260
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 07:14:34 +0800

    Prevent updating.

--- insert-answer.php
+++ insert-answer.php
@@ -1,29 +1,22 @@
-<?php 
+<?php
 include 'includes/session.php';
 include 'includes/conn.php';
 
 if (isset($_POST['submit'])) {
     $participant = $_SESSION['participant'];
-    $question_id = $_POST['question_id'];
-    $answer = $_POST['answer'];
-    $sql = "SELECT * FROM answer WHERE participant_id = '$participant' AND question_id = '$quesion_id'";
-    $query = $conn->query($sql);
+    $quesion_id  = $_POST['question_id'];
+    $answer      = $_POST['answer'];
+    $sql         = "SELECT * FROM answer WHERE participant_id = '$participant' AND question_id = '$quesion_id'";
+    $query       = $conn->query($sql);
     if ($query->num_rows > 0) {
         header('location: home');
     } else {
-        
-        $sql = "INSERT INTO answer (participant_id, question_id, answer) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("iis", $participant, $question_id, $answer);
-        if ($stmt->execute()) {
-            // Redirect to avoid resubmission
-            header("Location: index.php");
-            exit();
-        } else {
-            echo "Error: " . $conn->error;
-        }
 
-        $stmt->close();
-        $conn->close();
+        $sql = "INSERT INTO answer (participant_id, question_id, answer) VALUES ('$participant', '$quesion_id', '$answer')";
+        if ($conn->query($sql) === TRUE) {
+            header('location: home');
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
 }
