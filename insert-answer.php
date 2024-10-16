8fc9a875662538940e097e061462165c4a424662
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 06:26:32 +0800

    Fix another bug.

--- insert-answer.php
+++ insert-answer.php
@@ -4,7 +4,7 @@ include 'includes/conn.php';
 
 if (isset($_POST['submit'])) {
     $participant = $_SESSION['participant'];
-    $quesion_id = $_POST['question_id'];
+    $question_id = $_POST['question_id'];
     $answer = $_POST['answer'];
     $sql = "SELECT * FROM answer WHERE participant_id = '$participant' AND question_id = '$quesion_id'";
     $query = $conn->query($sql);
@@ -14,7 +14,7 @@ if (isset($_POST['submit'])) {
         
         $sql = "INSERT INTO answer (participant_id, question_id, answer) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("iis", $user_id, $question_id, $answer);
+        $stmt->bind_param("iis", $participant, $question_id, $answer);
         if ($stmt->execute()) {
             // Redirect to avoid resubmission
             header("Location: index.php");
