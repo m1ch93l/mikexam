12aae34e882187d635ba5778dc5cb1d7c8a58d66
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 22:04:03 +0800

    Fix bug in next func.

--- admin/next.php
+++ admin/next.php
@@ -2,26 +2,18 @@
 
 include '../includes/database.php';
 
-$next  = $_GET['next'];
-$sql   = "SELECT * FROM answer WHERE question_id = :next";
-$stmt  = $conn->prepare($sql);
+$next = $_GET['next'];
+
+$sql  = "UPDATE question SET is_active = 0 WHERE id = :next";
+$stmt = $conn->prepare($sql);
 $stmt->bindParam(':next', $next);
 $stmt->execute();
 
-if ($stmt->rowCount() > 0) {
+$sql  = "UPDATE question SET is_active = 1 WHERE id = :next + 1";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':next', $next);
+if ($stmt->execute()) {
     header('location: home');
 } else {
-    $sql = "UPDATE question SET is_active = 0 WHERE id = :next";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':next', $next);
-    $stmt->execute();
-
-    $sql = "UPDATE question SET is_active = 1 WHERE id = :next + 1";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':next', $next);
-    if ($stmt->execute()) {
-        header('location: home');
-    } else {
-        echo "Error: " . $stmt->errorInfo()[2];
-    }
+    echo "Error: " . $stmt->errorInfo()[2];
 }
