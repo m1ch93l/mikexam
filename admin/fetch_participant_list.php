c3843d8ce6ab9c5af319d0c02cb246b0d70714e7
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 20:08:10 +0800

    Update fetch data.

--- admin/fetch_participant_list.php
+++ admin/fetch_participant_list.php
@@ -1,16 +1,21 @@
-<?php include '../includes/conn.php'; ?>
+<?php include '../includes/database.php'; ?>
 <div class="row">
     <div class="col-2"></div>
     <div class="col-8">
+        
         <?php
+
         $sql   = "SELECT par.id, par.fullname, (SELECT COUNT(*) FROM (SELECT q.id as question_id, q.correct_answer, a.answer as answer, par2.id as part_id 
           FROM question q 
           LEFT JOIN answer a ON q.id = a.question_id 
           LEFT JOIN participant par2 ON a.participant_id = par2.id
           WHERE par2.id = par.id) as latest_result WHERE latest_result.answer = latest_result.correct_answer) as total_correct
           FROM participant par ORDER BY total_correct DESC";
+        $stmt  = $conn->prepare($sql);
+        $stmt->execute();
         $query = $conn->query($sql);
-        while ($row = $query->fetch_assoc()) { ?>
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
             <div class="card mb-1">
                 <div class="card-body d-flex justify-content-between">
                     <h5 class="card-title text-capitalize"><?= $row['fullname'] ?></h5>
@@ -18,6 +23,7 @@
                 </div>
             </div>
         <?php } ?>
+
     </div>
     <div class="col-2"></div>
 </div>
