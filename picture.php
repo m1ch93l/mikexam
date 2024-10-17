410fad4e697f8dd814f0785ef18c2728ad431f69
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 22:20:57 +0800

    Add picture page.

---
+++ picture.php
@@ -0,0 +1,22 @@
+<?php
+if ($question['id'] == 46) { ?>
+    <div class="text-center">
+        <img src="public/1.png" alt="46" class="w-50">
+    </div>
+<?php } else if ($question['id'] == 47) { ?>
+        <div class="text-center">
+            <img src="public/47.png" alt="47" class="w-50">
+        </div>
+<?php } else if ($question['id'] == 48) { ?>
+            <div class="text-center">
+                <img src="public/48.png" alt="48" class="w-50">
+            </div>
+<?php } else if ($question['id'] == 49) { ?>
+                <div class="text-center">
+                    <img src="public/49.png" alt="49" class="w-50">
+                </div>
+<?php } else if ($question['id'] == 50) { ?>
+                    <div class="text-center">
+                        <img src="public/50.png" alt="50" class="w-50">
+                    </div>
+<?php } ?>
