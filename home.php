135c7d7c4c6bebbc9c27cb7231e61463c451f218
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-17 21:30:03 +0800

    Update home.

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
+
 <body>
     <header class="bg-dark py-3 text-white text-center">
         mikexam
@@ -9,53 +10,57 @@
     <div class="container-md">
         <h5 class="text-center text-capitalize mb-1">Welcome, <?= $_SESSION['fullname'] ?></h5>
         <?php
-        $sql      = "SELECT * FROM question WHERE is_active = 1 ORDER BY id ASC";
-        $query    = $conn->query($sql);
-        $questions = $query->fetch_all(MYSQLI_ASSOC);
+        $sql  = "SELECT * FROM question WHERE is_active = 1 ORDER BY id ASC";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         $answered = [];
-        $sql      = "SELECT question_id FROM answer WHERE participant_id = " . $_SESSION['participant'];
-        $query    = $conn->query($sql);
-        $answered = $query->fetch_all(MYSQLI_ASSOC);
+        $sql      = "SELECT question_id FROM answer WHERE participant_id = :participant";
+        $stmt     = $conn->prepare($sql);
+        $stmt->bindParam(':participant', $_SESSION['participant']);
+        $stmt->execute();
+        $answered = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
         <?php foreach ($questions as $question) {
             if (!in_array($question['id'], array_column($answered, 'question_id'))) { ?>
-        <h4 class="text-center text-capitalize mb-2"><?= $question['id'] ?>. <?= $question['question'] ?></h4>
+                <h4 class="text-center text-capitalize mb-2"><?= $question['id'] ?>. <?= $question['question'] ?></h4>
 
-        <form action="insert-answer" method="post">
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <input type="hidden" name="question_id" value="<?= $question['id'] ?>">
-                <div class="col">
-                    <input type="radio" class="btn-check mx-auto mb-3 text-uppercase" value="a" id="btnradio1"
-                        name="answer">
-                    <label class="btn btn-primary btn-lg d-block mx-auto" for="btnradio1"
-                        onclick="changeColor(this)">A</label>
-                </div>
-                <div class="col">
-                    <input type="radio" class="btn-check mx-auto mb-3 text-uppercase" value="b" id="btnradio2"
-                        name="answer">
-                    <label class="btn btn-success btn-lg d-block mx-auto" for="btnradio2"
-                        onclick="changeColor(this)">B</label>
-                </div>
-                <div class="col">
-                    <input type="radio" class="btn-check mb-3 text-uppercase" value="c" id="btnradio3" name="answer">
-                    <label class="btn btn-danger btn-lg d-block mx-auto d-block" for="btnradio3"
-                        onclick="changeColor(this)">C</label>
-                </div>
-                <div class="col">
-                    <input type="radio" class="btn-check mb-3 text-uppercase" value="d" id="btnradio4" name="answer"
-                        onclick="changeColor(this)">
-                    <label class="btn btn-info btn-lg d-block mx-auto d-block" for="btnradio4"
-                        onclick="changeColor(this)">D</label>
-                </div>
-            </div>
-            <div class="text-center mt-5">
-                <button type="submit" name="submit" class="btn btn-dark btn-lg">Submit</button>
-            </div>
-        </form>
-        <?php } else { ?>
-        <h4 class="text-center text-capitalize mb-2"><?= $question['id'] ?>. <?= $question['question'] ?></h4>
-        <p class="text-center">You have already answered this question</p>
-        <?php } ?>
+                <form action="crud" method="post">
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <input type="hidden" name="question_id" value="<?= $question['id'] ?>">
+                        <div class="col">
+                            <input type="radio" class="btn-check mx-auto mb-3 text-uppercase" value="a" id="btnradio1"
+                                name="answer">
+                            <label class="btn btn-primary btn-lg d-block mx-auto" for="btnradio1"
+                                onclick="changeColor(this)">A</label>
+                        </div>
+                        <div class="col">
+                            <input type="radio" class="btn-check mx-auto mb-3 text-uppercase" value="b" id="btnradio2"
+                                name="answer">
+                            <label class="btn btn-success btn-lg d-block mx-auto" for="btnradio2"
+                                onclick="changeColor(this)">B</label>
+                        </div>
+                        <div class="col">
+                            <input type="radio" class="btn-check mb-3 text-uppercase" value="c" id="btnradio3" name="answer">
+                            <label class="btn btn-danger btn-lg d-block mx-auto d-block" for="btnradio3"
+                                onclick="changeColor(this)">C</label>
+                        </div>
+                        <div class="col">
+                            <input type="radio" class="btn-check mb-3 text-uppercase" value="d" id="btnradio4" name="answer"
+                                onclick="changeColor(this)">
+                            <label class="btn btn-info btn-lg d-block mx-auto d-block" for="btnradio4"
+                                onclick="changeColor(this)">D</label>
+                        </div>
+                    </div>
+                    <div class="text-center mt-5">
+                        <button type="submit" name="insert-answer" class="btn btn-dark btn-lg">Submit</button>
+                    </div>
+                </form>
+            <?php } else { ?>
+                <h4 class="text-center text-capitalize mb-2"><?= $question['id'] ?>. <?= $question['question'] ?></h4>
+                <p class="text-center">You have already answered this question</p>
+            <?php } ?>
         <?php } ?>
     </div>
     <script>
