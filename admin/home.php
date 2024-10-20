e40ecedb05df9e3384d61582264985e5427c2532
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-21 02:25:07 +0800

    Hide/Show student name.

--- admin/home.php
+++ admin/home.php
@@ -28,9 +28,11 @@ if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
         <a href="logout" type="button"
             class="text-decoration-none text-white float-end h6 me-3 border p-1 text-capitalize">logout</a>
     </header>
-    <a class="text-decoration-none text-dark ms-3" href="navigator">.</a>
+    <div class="container-fluid mb-3">
+        <a class="text-decoration-none text-dark ms-3" href="navigator">.</a>
+        <button class="btn btn-dark btn-sm me-5 float-end" id="toggleButton">View</button>
+    </div>
     <main>
-        <button class="btn btn-dark float-end me-5" id="toggleButton">View</button>
         <div class="container-md" id="participant-list">
             
         </div>
@@ -52,23 +54,8 @@ if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
             fetch(url)
                 .then(response => response.text())
                 .then(data => participantList.innerHTML = data);
-        }, 5000);
+        }, 1000);
     </script>
-
-    <!-- <script>
-        function fetch_participant_list() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("participant-list").innerHTML = this.responseText;
-                }
-            };
-            xhttp.open("GET", "fetch_participant_list.php", true);
-            xhttp.send();
-        }
-
-        setInterval(fetch_participant_list, 23000);
-    </script> -->
 
 </body>
 
