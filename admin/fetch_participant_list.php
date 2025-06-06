27d020bfe77ca486abff9312061c10be5bdadf3c
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-06 13:32:04 +0800

    answers fixed

--- admin/fetch_participant_list.php
+++ admin/fetch_participant_list.php
@@ -3,25 +3,27 @@
     <div class="col-2"></div>
     <div class="col-md-8 col-sm-12">
         <div id="participant-list">
-            <?php
-            $sql  = "SELECT par.id, par.fullname, (SELECT COUNT(*) FROM (SELECT q.id as question_id, q.correct_answer, a.answer as answer, par2.id as part_id 
-              FROM question q 
-              LEFT JOIN answer a ON q.id = a.question_id 
-              LEFT JOIN participant par2 ON a.participant_id = par2.id
-              WHERE par2.id = par.id) as latest_result WHERE latest_result.answer = latest_result.correct_answer) as total_correct
-              FROM participant par ORDER BY total_correct DESC";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $query = $conn->query($sql);
+        <?php
+            $sql = "SELECT participant.id
+                AS participant_id, participant.fullname,
+                COUNT(CASE WHEN a.answer = q.correct_answer THEN 1 END) AS total_correct
+                FROM participant participant
+                LEFT JOIN answer a ON participant.id = a.participant_id
+                LEFT JOIN question q ON a.question_id = q.id
+                GROUP BY participant.id, participant.fullname
+                ORDER BY total_correct DESC";
 
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
                 <div class="card mb-1 shadow">
                     <div class="card-body d-flex justify-content-between">
-                        <h5 class="card-title text-capitalize"><?= $row['fullname'] ?></h5>
+                        <h5 class="card-title text-capitalize"><?= htmlspecialchars($row['fullname']) ?></h5>
                         <h5 class="me-3">Total correct answers: <?= $row['total_correct'] ?></h5>
                     </div>
                 </div>
-            <?php } ?>
+        <?php } ?>
         </div>
     </div>
     <div class="col-2"></div>
